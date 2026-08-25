68b0489b02053ad9b5168510d17d8483564bf8c7
zitech-itsolutiuons <zitechurityitsolutions@gmail.com>
2026-08-25 17:49:28 +0100

    Modify next.config.js for standalone output
    Updated next.config.js for server deployment configuration.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   experimental: {
+//     serverActions: {
+//       bodySizeLimit: '5mb', // allow CSV question imports
+//     },
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '5mb', // allow CSV question imports
-    },
-  },
-};
+  // For server deployment (with database)
+  output: 'standalone',
+  // For static export (no database)
+  // output: 'export',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
